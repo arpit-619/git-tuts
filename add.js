cb3379e6803eda39f92d49b74aaee49bf523c4fd
Arpit Alne <arpit.alne@perficient.com>
2021-08-05 10:13:19 +0530

    Total marks

--- add.js
+++ add.js
@@ -3,4 +3,11 @@ function add(a, b) {
     console.log(a+b);
 }
 
-add(a,b)
+add(a, b);
+
+let arr = [1, 3, 5, 78, 3];
+let sum = 0;
+for (i = 0; i < arr.length; i++){
+    sum += arr[i];
+}
+console.log(sum);
